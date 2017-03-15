5b42f07a8ab572d55ae05c18ef24ddf26689b341
puchk <kpuchalski@gmail.com>
2017-03-15 01:48:57 -0700

    Added comments and minor js and css changes

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,36 +1,6 @@
 var topics = ["LeBron James", "Kevin Durant", "James Harden", "Kawhi Leonard", "Russell Westbrook", "Steph Curry", "Chris Paul"];
 
-function displayGifs() {
-  var player = $(this).attr("data-player");
-  var queryURL = "http://api.giphy.com/v1/gifs/search?q=" + player + "&api_key=dc6zaTOxFJmzC&limit=10";
-  $.ajax({
-      url: queryURL,
-      method: "GET"
-    })
-        .done(function(response) {
-          $("#placeGifs").empty();
-
-        var playerData = response.data;
-
-        for (var i = 0; i < playerData.length; i++) {
-          var gifDiv = $("<div class='content'>");
-          var rating = $("<p class='rating'>").text("Rating: " + playerData[i].rating);
-          var stillImage = playerData[i].images.fixed_height_still.url;
-          var movingImage = playerData[i].images.fixed_height.url;
-          var image = $("<img>")
-              image.attr("src", stillImage);
-              image.attr("data-still", stillImage);
-              image.attr("data-moving", movingImage);
-              image.attr("data-isGifPlaying", "no");
-              image.addClass("playerGif");
-            gifDiv.append(image);
-            gifDiv.append(rating);
-        
-          $("#placeGifs").prepend(gifDiv);
-        }
-        })
-}
-
+// Creates a button and appends it to other buttons
 function addButtons(names){
   var a = $("<button>");
   a.addClass("player");
@@ -39,6 +9,7 @@ function addButtons(names){
    $("#placeButtons").append(a);
 }
 
+// Uses addButtons function to create buttons for players in topics array
 function startButtons(){
   for (var i=0; i<topics.length; i++){
     addButtons(topics[i]);
@@ -47,16 +18,66 @@ function startButtons(){
 
 $("#submitButton").on("click", function(){
   var userInput = $("#addPlayerInput").val().trim();
-  addButtons(userInput);
-  $("#addPlayerInput").val('');
+  
+  // If input is empty
+  if ($("#addPlayerInput").val().trim() === ""){
+    alert("Please enter a name");
+  }
+  // If input has a value of the same button (is case sensitive)
+  else if ($.inArray(userInput, topics) != -1) {
+    alert("Player already entered");
+  }
+  else {
+    // Create a button with input value using addButtons function 
+    addButtons(userInput);
+    // Adds input to player list
+    topics.push(userInput);
+    // Empties input
+    $("#addPlayerInput").val('');
+  }
 });
 
+// Gathers data from api and places data in divs
+function displayGifs() {
+  var player = $(this).attr("data-player");
+  var queryURL = "http://api.giphy.com/v1/gifs/search?q=" + player + "&api_key=dc6zaTOxFJmzC&limit=10";
+  $.ajax({
+      url: queryURL,
+      method: "GET"
+    })
+        .done(function(response) {
+          // Removes any gifs already placed so shows one player at a time
+          $("#placeGifs").empty();
+
+        var playerData = response.data;
+
+        // For each data object: create a gif div and append data inside
+        for (var i = 0; i < playerData.length; i++) {
+          var gifDiv = $("<div class='content'>");
+          var rating = $("<p class='rating'>").text("Rating: " + playerData[i].rating);
+          var stillImage = playerData[i].images.fixed_height_still.url;
+          var movingImage = playerData[i].images.fixed_height.url;
+          var image = $("<img>")
+              image.attr({
+                "src": stillImage, 
+                "data-still": stillImage,
+                "data-moving": movingImage,
+                "data-isGifPlaying": "no"});
+              image.addClass("playerGif");
+            gifDiv.append(image);
+            gifDiv.append(rating);
+        // Place gif div in html
+          $("#placeGifs").prepend(gifDiv);
+        }
+        })
+}
+
+// Uses displayGifs function when a player button is clicked
 $(document).on("click", ".player", displayGifs);
 
-  //Start and Pause Gifs 
+//Start and Pause Gifs 
 $(document).on("click", ".playerGif", function(){
 	var isPlaying = $(this).attr("data-isGifPlaying");
-	console.log(isPlaying==="no");
 	if (isPlaying === "no"){
 		$(this).attr("src", $(this).attr("data-moving"));
 		$(this).attr("data-isGifPlaying", "yes");
